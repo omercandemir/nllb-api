cce4410b918956078e1198bb9648b2339ae73165
Ömercan Demir <reatweb@gmail.com>
2024-08-30 10:28:42 +0300

    added global languages and typesafe

--- server/features/types/languages.py
+++ server/features/types/languages.py
@@ -1,5 +1,56 @@
-from typing import TypedDict
+from typing import Literal, TypedDict
 
+# Define the Languages type using Literal
+Languages = Literal[
+    'ace_Arab',
+    'ace_Latn',
+    'acm_Arab',
+    'acq_Arab',
+    'aeb_Arab',
+    'afr_Latn',
+    'ajp_Arab',
+    'aka_Latn',
+    'amh_Ethi',
+    'apc_Arab',
+    'arb_Arab',
+    'arb_Latn',
+    'ars_Arab',
+    'ary_Arab',
+    'arz_Arab',
+    'asm_Beng',
+    'ast_Latn',
+    'awa_Deva',
+    'ayr_Latn',
+    'azb_Arab',
+    'azj_Latn',
+    'bak_Cyrl',
+    'bam_Latn',
+    'ban_Latn',
+    'bel_Cyrl',
+    'bem_Latn',
+    'ben_Beng',
+    'bho_Deva',
+    'bjn_Arab',
+    'bjn_Latn',
+    'bod_Tibt',
+    'bos_Latn',
+    'bug_Latn',
+    'bul_Cyrl',
+    'cat_Latn',
+    'ceb_Latn',
+    'ces_Latn',
+    'cjk_Latn',
+    'ckb_Arab',
+    'crh_Latn',
+    'cym_Latn',
+    'dan_Latn',
+    'deu_Latn',
+    'dik_Latn',
+    'eng_Latn',
+    'tur_Latn'
+]
+
+# Define the TranslatorOptions TypedDict
 class TranslatorOptions(TypedDict, total=False):
     ace_Arab: str
     ace_Latn: str
@@ -46,4 +97,3 @@ class TranslatorOptions(TypedDict, total=False):
     dan_Latn: str
     deu_Latn: str
     dik_Latn: str
-    dyu_Latn: str
